a0e4616c7ffa5fd5efdb6c1935ce3a0af0719706
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-10-10 23:59:14 +0200

    Implement user management API endpoints

---
+++ api/routers/user_api.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter
+from typing import List, Union
+from api.models.users import User
+
+router = APIRouter()
+user_model = User()
+
+
+@router.get("/users")
+async def get_all_users() -> List[dict]:
+    return user_model.get_all_users_data()
+
+
+@router.get("/users/me")
+async def get_current_user() -> dict:
+    return {"user_id": "the current user"}
+
+
+@router.get("/users/{user_id}")
+async def get_user_by_id(user_id: int) -> dict:
+    return user_model.get_user_by_id(user_id)
+
+
+@router.get("/users/")
+async def get_limit_user(
+    skip: Union[int, None] = 0,
+    limit: Union[int, None] = 5,
+    name: Union[str, None] = None
+) -> Union[str, dict, list]:
+    if name:
+        return user_model.get_user_by_username(name)
+    return user_model.get_all_users_data(skip, skip + limit)
+
+
+@router.post("/user")
+async def create_new_user(
+    username: str, email: str, password: str,
+    date_of_birth: Union[str, None] = None,
+    description: Union[str, None] = None
+):
+    if user_model.check_if_user_exists(username=username, email=email):
+        return {"error": "User already exists"}
+
+    data = user_model.insert_new_user(
+        username=username, email=email, hashed_password=password,
+        date_of_birth=date_of_birth, description=description
+    )
+
+    return data
+
+
+# @router.get("/user-name/")
+# async def get_user_by_name(
+#     name: str
+# ) -> Union[dict, str]:
+#     return user.user_by_name(name)
