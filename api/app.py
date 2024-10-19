7f2e01c6e0bc7ba02ce309f9e456a4c1d658b09f
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-10-19 08:08:11 +0200

    add Docstring, import note class

--- api/app.py
+++ api/app.py
@@ -1,20 +1,25 @@
 """app.py"""
 
-from typing import Union
+# from typing import Union
 from fastapi import APIRouter
 from api.models.users import User
+from api.models.notes import Note
+
 
 router = APIRouter()
 user_model = User()
+note_model = Note()
 
 
 @router.get("/")
 async def root():
+    """root"""
     return {"message": "Hello World"}
 
 
 @router.get("/home")
 async def home():
+    """Home Page"""
     return {"message": "Welcome in Home"}
 
 
